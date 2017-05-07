f3214d05a4a57bc6d59219daa5c963b2c45561ba
ahmedgf100 <ahmedgf100@gmail.com>
2017-05-07 22:49:14 +0200

    -a

--- src/main/java/com/Fun/Course/CourseRepository.java
+++ src/main/java/com/Fun/Course/CourseRepository.java
@@ -1,7 +1,6 @@
 package com.Fun.Course;
 
 import org.springframework.data.repository.CrudRepository;
-import org.yaml.snakeyaml.events.Event.ID;
 import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course,Integer>{
--- src/main/java/com/Fun/Teacher/TeacherRepository.java
+++ src/main/java/com/Fun/Teacher/TeacherRepository.java
@@ -1,7 +1,6 @@
 package com.Fun.Teacher;
 
 import org.springframework.data.repository.CrudRepository;
-import org.yaml.snakeyaml.events.Event.ID;
 
 public interface TeacherRepository extends CrudRepository<Teacher,String>{
 
